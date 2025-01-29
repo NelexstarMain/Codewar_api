4f3b01cb634df7c6d639fba553741b12936a6ba7
Nelexstar <kbartlewski5@gmail.com>
2025-01-29 16:29:43 +0100

    Add: New class for kata information

--- scr/chalanges.py
+++ scr/chalanges.py
@@ -1,7 +1,7 @@
 import requests
 
-class GetInformations:
-    def __init__(self, user) -> None:
+class UserKataInfo:
+    def __init__(self, user: str) -> None:
         self.USER: str = user
         self.URL: str = f"https://www.codewars.com/api/v1/users/{self.USER}/code-challenges/completed?page=0"
         self.id_list: list = []
@@ -14,11 +14,26 @@ class GetInformations:
             completed = data.get('data')
             for chalanege in completed:
                 self.id_list.append(chalanege.get('id'))
-
         else:
             print(f"Error Key: {response.status_code}")
 
-if __name__=="__main__":
-    p = GetInformations("Elon_Musk")
-    p.get()
-    print(p.id_list)
+class KataInfo:
+    def __init__(self, id: int) -> None:
+        self.ID = id
+        self.URL = f"https://www.codewars.com/api/v1/code-challenges/{self.ID}"
+        self.title: str = ""
+        self.info: str = ""
+        self.kyu: str = ""
+
+    def get(self) -> None:
+        response = requests.get(self.URL)
+
+        if response.status_code == 200:
+            data = response.json()
+
+            self.title = data.get('slug')
+            self.info = data.get('description')
+            self.kyu = data.get('rank').get('name')
+
+        else:
+            print(f"Error Key: {response.status_code}")
