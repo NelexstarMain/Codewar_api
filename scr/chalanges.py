53c0122f612b9fa0b359ccebc7baea326d019c06
Nelexstar <kbartlewski5@gmail.com>
2025-01-29 12:39:33 +0100

    Fix: Create OOP version

--- scr/chalanges.py
+++ scr/chalanges.py
@@ -1,15 +1,24 @@
 import requests
 
-USER = "NelexstarMain"
-PAGE = 1
-URL = f"https://www.codewars.com/api/v1/users/{USER}/code-challenges/completed?page=0"
+class GetInformations:
+    def __init__(self, user) -> None:
+        self.USER: str = user
+        self.URL: str = f"https://www.codewars.com/api/v1/users/{self.USER}/code-challenges/completed?page=0"
+        self.id_list: list = []
 
-response = requests.get(URL)
+    def get(self) -> None:   
+        response = requests.get(self.URL)
 
-if response.status_code == 200:
-    data = response.json()
-    completed = data.get('data')
-    for chalanege in completed:
-        print(chalanege.get('id'))
-else:
-    print(f"Nie udało się pobrać danych. Kod błędu: {response.status_code}")
+        if response.status_code == 200:
+            data = response.json()
+            completed = data.get('data')
+            for chalanege in completed:
+                self.id_list.append(chalanege.get('id'))
+
+        else:
+            print(f"Error Key: {response.status_code}")
+
+if __name__=="__main__":
+    p = GetInformations("Elon_Musk")
+    p.get()
+    print(p.id_list)
